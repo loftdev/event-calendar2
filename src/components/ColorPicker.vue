fb7284beb37d8fc516017532243ff3ec2f7fe12a
lofty mier <loftylaisonmier@gmail.com>
2019-09-30 14:43:53 +0900

    added colorpicer

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -1,11 +1,27 @@
 <template>
-    
+    <div class="picker-main"> 
+        <h4 class="header">Select event theme</h4>
+        <div class="color-picker">
+          <div class="color" v-for="(theme, index) in colors" :key="index" @click="selectColor(theme)" :class="{selected: color === theme, [theme]: theme}"></div>
+        </div>
+      </div>
 </template>
 
 <script>
-export default {
-    
-}
+    export default {
+      name: 'ColorPicker',
+      props: ['color'],
+      data(){
+        return {
+          colors: ['red', 'green', 'blue', 'orange']
+        }
+      },
+      methods: {
+        selectColor(color){
+          this.$emit('colorPicked', color);
+        }
+      }
+    }
 </script>
 
 <style scoped>
